f80c258158e9cbb8e635f100a6b7471fc7ec72fd
Bati <95726581+Bati2ra@users.noreply.github.com>
2025-09-11 14:17:43 -0300

    fix: held item for gecko player

---
+++ astradia-client/src/main/java/com/astradia/render/layer/GeckoPlayerHeldItemLayer.java
@@ -0,0 +1,106 @@
+package com.astradia.render.layer;
+
+import com.astradia.AstradiaClient;
+import com.astradia.player.PlayerBodyProportions;
+import com.astradia.player.body.BodyPartProportion;
+import com.astradia.player.body.BodyProportionsConfig;
+import com.astradia.render.player.PlayerRenderer;
+import com.astradia.utils.AstradiaPlayerEntityRenderState;
+import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
+import net.minecraft.client.render.item.ItemRenderState;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.Arm;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.RotationAxis;
+import org.jetbrains.annotations.Nullable;
+import org.joml.Matrix4f;
+import org.joml.Vector3f;
+import software.bernie.geckolib.animatable.GeoAnimatable;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
+import software.bernie.geckolib.renderer.base.GeoRenderState;
+import software.bernie.geckolib.renderer.base.GeoRenderer;
+import software.bernie.geckolib.renderer.layer.GeoRenderLayer;
+
+import java.util.UUID;
+
+public class GeckoPlayerHeldItemLayer<T extends LivingEntity & GeoAnimatable, O, R extends PlayerEntityRenderState & GeoRenderState>  extends GeoRenderLayer<T, O, R> {
+
+    public GeckoPlayerHeldItemLayer(GeoRenderer<T, O, R> renderer) {
+        super(renderer);
+    }
+
+    protected void renderItem(R playerEntityRenderState, ItemRenderState itemRenderState, Arm arm, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
+        if (!itemRenderState.isEmpty()) {
+            Hand hand = arm == playerEntityRenderState.mainArm ? Hand.MAIN_HAND : Hand.OFF_HAND;
+            if (playerEntityRenderState.isUsingItem && playerEntityRenderState.activeHand == hand && playerEntityRenderState.handSwingProgress < 1.0E-5F && !playerEntityRenderState.spyglassState.isEmpty()) {
+                this.renderSpyglass(playerEntityRenderState, playerEntityRenderState.spyglassState, arm, matrixStack, vertexConsumerProvider, i);
+            } else {
+                matrixStack.push();
+
+                var playerRenderer = ((PlayerRenderer) renderer);
+                var itemBone = arm.equals(Arm.RIGHT) ? playerRenderer.rightHandItem : playerRenderer.leftHandItem;
+                UUID uuid = ((AstradiaPlayerEntityRenderState) playerEntityRenderState).getUuid();
+                PlayerBodyProportions playerProportions = AstradiaClient.getPlayerManager().getFromUuid(uuid).getProportions();
+                BodyProportionsConfig config = playerProportions.getConfig();
+                BodyPartProportion partProportion = arm.equals(Arm.RIGHT) ? config.rightArm : config.leftArm;
+                var scaleXZ = partProportion.getValue(BodyPartProportion.Axis.X) * config.width.getValue(BodyPartProportion.Axis.X);
+                var scaleY = partProportion.getValue(BodyPartProportion.Axis.Y) * config.height.getValue(BodyPartProportion.Axis.Y);
+                Vector3f inverseScale = new Vector3f(1.0f / scaleXZ,
+                        1.0f / scaleY,
+                        1.0f / scaleXZ);
+                Matrix4f scaledMatrix = itemBone.getWorldSpaceMatrix();
+                Matrix4f unscaledMatrix = new Matrix4f(scaledMatrix);
+                unscaledMatrix.scale(inverseScale); // elimina la escala acumulada
+
+                MatrixStack newMatrixStack = new MatrixStack();
+                newMatrixStack.peek().getNormalMatrix().mul(itemBone.getWorldSpaceNormal());
+                newMatrixStack.peek().getPositionMatrix().mul(unscaledMatrix);
+                newMatrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-90.0F));
+                itemRenderState.render(newMatrixStack, vertexConsumerProvider, i, OverlayTexture.DEFAULT_UV);
+                matrixStack.pop();
+            }
+        }
+    }
+
+    private void renderSpyglass(R playerEntityRenderState, ItemRenderState spyglassState, Arm arm, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light) {
+        var playerRenderer = ((PlayerRenderer) renderer);
+
+        var itemBone = arm.equals(Arm.RIGHT) ? playerRenderer.rightHandItem : playerRenderer.leftHandItem;
+        UUID uuid = ((AstradiaPlayerEntityRenderState) playerEntityRenderState).getUuid();
+        PlayerBodyProportions playerProportions = AstradiaClient.getPlayerManager().getFromUuid(uuid).getProportions();
+        BodyProportionsConfig config = playerProportions.getConfig();
+        BodyPartProportion partProportion = arm.equals(Arm.RIGHT) ? config.rightArm : config.leftArm;
+        Vector3f inverseScale = new Vector3f(1.0f / partProportion.getValue(BodyPartProportion.Axis.X),
+                1.0f / partProportion.getValue(BodyPartProportion.Axis.Y),
+                1.0f / partProportion.getValue(BodyPartProportion.Axis.X));
+
+        Matrix4f scaledMatrix = itemBone.getWorldSpaceMatrix();
+        Matrix4f unscaledMatrix = new Matrix4f(scaledMatrix);
+        unscaledMatrix.scale(inverseScale); // elimina la escala acumulada
+
+        MatrixStack newMatrixStack = new MatrixStack();
+        newMatrixStack.push();
+        newMatrixStack.peek().getNormalMatrix().mul(itemBone.getWorldSpaceNormal());
+        newMatrixStack.peek().getPositionMatrix().mul(unscaledMatrix);
+        newMatrixStack.multiply(RotationAxis.POSITIVE_X.rotationDegrees(-90.0F));
+        spyglassState.render(newMatrixStack, vertexConsumers, light, OverlayTexture.DEFAULT_UV);
+        newMatrixStack.pop();
+    }
+
+    @Override
+    public void render(R renderState, MatrixStack poseStack, BakedGeoModel bakedModel, @Nullable RenderLayer renderType, VertexConsumerProvider bufferSource, @Nullable VertexConsumer buffer, int packedLight, int packedOverlay, int renderColor) {
+
+        this.renderItem(renderState, renderState.rightHandItemState, Arm.RIGHT, poseStack, bufferSource, packedLight);
+        this.renderItem(renderState, renderState.leftHandItemState, Arm.LEFT, poseStack, bufferSource, packedLight);
+    }
+
+    public void render(MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i, R armedEntityRenderState, float f, float g) {
+        this.renderItem(armedEntityRenderState, armedEntityRenderState.rightHandItemState, Arm.RIGHT, matrixStack, vertexConsumerProvider, i);
+        this.renderItem(armedEntityRenderState, armedEntityRenderState.leftHandItemState, Arm.LEFT, matrixStack, vertexConsumerProvider, i);
+    }
+}
